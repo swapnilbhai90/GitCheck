45b850d87f536946d8f94f67244ae3654e369441
swapnilbhaisare <swapnilbhai90@gmail.com>
2017-08-06 17:38:45 +0530

    branch3_commit1

--- app/src/main/java/dagger/cloneplanet/com/daggertest/MainActivity.java
+++ app/src/main/java/dagger/cloneplanet/com/daggertest/MainActivity.java
@@ -1,20 +1,48 @@
 package dagger.cloneplanet.com.daggertest;
 
+import android.support.annotation.Nullable;
+import android.support.annotation.StyleRes;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView txt_helllo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt_helllo=(TextView)findViewById(R.id.txt_helllo);
-        txt_helllo.setText("bhaisare");
-        txt_helllo.setText("branch2");
 
+
+
+    }
+
+    public MainActivity() {
+        super();
+    }
+
+    @Override
+    public void setTheme(@StyleRes int resid) {
+        super.setTheme(resid);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public ActionBar getSupportActionBar() {
+        return super.getSupportActionBar();
+    }
+
+    @Override
+    public void setSupportActionBar(@Nullable Toolbar toolbar) {
+        super.setSupportActionBar(toolbar);
     }
 }
