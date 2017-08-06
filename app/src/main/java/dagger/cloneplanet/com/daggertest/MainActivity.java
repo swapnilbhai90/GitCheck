4b12b7e5c034bc1e588c15ff1cd469b969c368d2
swapnilbhaisare <swapnilbhai90@gmail.com>
2017-08-06 17:19:16 +0530

    branch1_commit1

--- app/src/main/java/dagger/cloneplanet/com/daggertest/MainActivity.java
+++ app/src/main/java/dagger/cloneplanet/com/daggertest/MainActivity.java
@@ -2,12 +2,18 @@ package dagger.cloneplanet.com.daggertest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txt_helllo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt_helllo=(TextView)findViewById(R.id.txt_helllo);
+        txt_helllo.setText("swapnil");
+
     }
 }
